6205554e91cc9671e2f54ebb88fd77de93c041fb
Bruno Lorenz Juchli <bruno.lorenz.juchli@erowa.com>
2018-06-15 16:35:17 +0200

    adding time reporting

--- NetCore/Run.bat
+++ NetCore/Run.bat
@@ -1,7 +1,12 @@
 @ECHO OFF
 
+SET StartDate=%date%
+SET StartTime=%time%
+
 :loop
-echo Run castle tests
+echo Run castle tests (.net core)
+echo %StartDate% %StartTime% (first run)
+echo %date% %time% (this run)
 .\NetCoreRepro\bin\Debug\netcoreapp2.1\win10-x64\NetCoreRepro.exe
 
 IF ERRORLEVEL 1 GOTO Failed
